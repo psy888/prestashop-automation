d5069fe49bb1e46e80a25fc57eecfea532a78695
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-17 14:45:29 +0200

    init

---
+++ src/main/java/page/object/HomePage.java
@@ -0,0 +1,11 @@
+package page.object;
+
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+
+@RequiredArgsConstructor
+public class HomePage {
+
+    private final WebDriver driver;
+
+}
