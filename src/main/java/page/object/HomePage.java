03ee4e53ed27eaa3ac1b5dec5342007fbb7c1f53
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-19 19:17:07 +0200

    clear and refactor HomePage class

--- src/main/java/page/object/HomePage.java
+++ src/main/java/page/object/HomePage.java
@@ -5,42 +5,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class HomePage {
 
-    private final WebDriver driver;
+    protected final WebDriver driver;
     //todo recheck selectors
     //currency
-    private By currentCurrency = By.cssSelector(".currency-selector.dropdown span.expand-more");
-    private By currency = By.xpath("//*[@id='_desktop_currency_selector']/div/ul/li/a");
+    protected By currentCurrency = By.cssSelector(".currency-selector.dropdown span.expand-more");
 
     //products
-    private By featuredProductsSection = By.cssSelector(".featured-products");
-    private By productsDiv = By.xpath("//div[@class='products']");
-    private By productArticle = By.xpath(".//article[itemtype='http://schema.org/Product']");
+    private By productsSection = By.cssSelector(".products");
 
     //product info
-    private By productPriceAndShippingDiv = By.xpath(".//div[@class='product-price-and-shipping']");
-    private By productPriceSpan = By.cssSelector("article .product-price-and-shipping span.price");
+    protected By productPriceSpan = By.cssSelector("article .product-price-and-shipping span.price");
 
 
-    private By getCurrencySelector(Currency currency) {
-        return By.xpath("//*[@id='_desktop_currency_selector']/div/ul/li/a[text()[contains(.,'" + currency.toString() + "')]]");
+    protected String[] getCurrencyFromString(String str) {
+        return str.split(" ", 2);
     }
 
-    private String[] getCurrencyFromString(String str) {
-        return str.split(" ",2);
-    }
-
-
-    private void selectCurrency(Currency currency) {
-        driver.findElement(getCurrencySelector(currency)).click();
-    }
-//#_desktop_currency_selector div.currency-selector.dropdown span.expand-more
 
     public String[] getCurrentCurrency() {
         return getCurrencyFromString(driver.findElement(currentCurrency).getText());
@@ -52,7 +38,7 @@ public class HomePage {
      * @return list of prices
      */
     public List<String> getItemsPrices() {
-        return driver.findElement(featuredProductsSection)
+        return driver.findElement(productsSection)
                 .findElements(productPriceSpan)
                 .stream()
                 .map(WebElement::getText)
