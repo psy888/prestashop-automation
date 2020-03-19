c74210248dbd42cb8d7fad3ae0b87407941df555
psy888 <tbilisi.zp.snab@gmail.com>
2020-03-19 20:18:12 +0200

    change regExp pattern, refactor select currency method

--- src/main/java/page/object/SearchResultPage.java
+++ src/main/java/page/object/SearchResultPage.java
@@ -3,30 +3,27 @@ package page.object;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class SearchResultPage extends HomePage {
 
-//    private final WebDriver driver;
-
     //currency
     private By currentCurrency = By.cssSelector(".currency-selector.dropdown span.expand-more");
-    private By currency = By.xpath("//*[@id='_desktop_currency_selector']/div/ul/li/a");
 
     ///search
     private By searchInput = By.cssSelector("#search_widget input[type='text']");
     private By searchButton = By.cssSelector("#search_widget button[type='submit']");
 
+
     //products
-    private By featuredProductsSection = By.cssSelector(".featured-products");
     private By totalProductsCountP = By.cssSelector("#products .total-products");
     private By productArticle = By.cssSelector("#products article.product-miniature");
 
     //product info
-    private By productPriceAndShippingDiv = By.xpath(".//div[@class='product-price-and-shipping']");
     private By productPriceSpan = By.cssSelector("article .product-price-and-shipping span.price");
 
-
-    private int itemsFound;
 
     public SearchResultPage(WebDriver driver) {
         super(driver);
@@ -46,13 +43,22 @@ public class SearchResultPage extends HomePage {
     }
 
 
-
     public void selectCurrency(Currency currency) {
+        showCurrencyDropdown();
         driver.findElement(getCurrencySelector(currency)).click();
     }
 
+    private void showCurrencyDropdown() {
+        driver.findElement(currentCurrency).click();
+    }
+
     public int getTotalProductsCount() {
-        return Integer.parseInt(driver.findElement(totalProductsCountP).getText().split("\\d*")[0]);
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(driver.findElement(totalProductsCountP).getText());
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group());
+        }
+        return -1;
     }
 
     public int countFoundProducts() {
